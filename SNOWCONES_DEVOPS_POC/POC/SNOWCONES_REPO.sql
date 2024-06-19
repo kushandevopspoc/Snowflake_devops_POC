421ed53cfcb782e5bb2ab28d427779e8c3c7c201
kushandevopspoc <kushandevopspoc@gmail.com>
2024-06-19 13:31:07 -0400

    Update SNOWCONES_REPO.sql

--- SNOWCONES_DEVOPS_POC/POC/SNOWCONES_REPO.sql
+++ SNOWCONES_DEVOPS_POC/POC/SNOWCONES_REPO.sql
@@ -1,1 +1,38 @@
+-- ALWAYS USE ACCOUN ADMIN LEVEL ROLE
+USE ROLE ACCOUNTADMIN;
 
+-- CREATING A SMALL WAREHOUSE FOR COMPUTE NEEDS
+CREATE WAREHOUSE IF NOT EXISTS POC_WH WAREHOUSE_SIZE = XSMALL, AUTO_SUSPEND = 300, AUTO_RESUME= TRUE;
+
+-- Separate database for git repository
+CREATE DATABASE IF NOT EXISTS SNOWCONES_DEVOPS_POC;
+
+-- Schema for the Github integrations
+CREATE SCHEMA IF NOT EXISTS SNOWCONES_DEVOPS_POC.POC;
+
+-- Setting context:
+
+USE DATABASE SNOWCONES_DEVOPS_POC;
+
+USE SCHEMA SNOWCONES_DEVOPS_POC.POC;
+
+-- API integration is needed for GitHub integration
+-- For enterprise refer below document for creating the api integration
+-- https://docs.snowflake.com/en/sql-reference/sql/create-api-integration#access-control-requirements
+CREATE OR REPLACE API INTEGRATION GIT_API_INTEGRATION
+  API_PROVIDER = git_https_api
+  API_ALLOWED_PREFIXES = ('https://github.com/kushandevopspoc') -- INSERT THE GITHUB USERNAME HERE
+  ENABLED = TRUE;
+
+-- Git repository object is similar to external stage
+-- For enterprise refer below document for creating the api integration
+-- https://docs.snowflake.com/en/sql-reference/sql/create-git-repository
+CREATE OR REPLACE GIT REPOSITORY GIT_REPO_STG
+  API_INTEGRATION = GIT_API_INTEGRATION
+  ORIGIN = 'https://github.com/kushandevopspoc/Snowflake_devops_POC/'; -- INSERT URL OF REPO HERE
+
+-- Repo refresh command
+alter git repository SNOWCONES_DEVOPS_POC.POC.GIT_REPO_STG fetch;
+
+-- Showing branches in the specific repo:
+show git branches in GIT_REPO_STG;
