536d9b881066f428611cf2389394c1aa43480285
kushandevopspoc <kushandevopspoc@gmail.com>
2024-06-26 11:44:03 -0400

    Update T_Devops_Demo.sql

--- PROD_US_DOMAINS/STAGING_DATA/Tables/T_Devops_Demo.sql
+++ PROD_US_DOMAINS/STAGING_DATA/Tables/T_Devops_Demo.sql
@@ -8,6 +8,5 @@
 CREATE OR ALTER TABLE PROD_US_DOMAINS.STAGING_DATA.EMPLOYEE (
   EMP_ID NUMBER(38,0),
   EMP_NAME varchar,
-  EMP_BRANCH varchar,
-  EMP_SAL NUMBER(38,0)
+  EMP_BRANCH varchar
 ) data_retention_time_in_days = 1;
