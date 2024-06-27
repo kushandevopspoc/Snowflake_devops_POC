6fec11e5351b694cbd62646f8ad75524772b0bb2
kushandevopspoc <kushandevopspoc@gmail.com>
2024-06-27 15:05:13 -0400

    Update SNOWCONES_REPO.sql

--- SNOWCONES_DEVOPS_POC/POC/SNOWCONES_REPO.sql
+++ SNOWCONES_DEVOPS_POC/POC/SNOWCONES_REPO.sql
@@ -1,4 +1,4 @@
--- ALWAYS USE ACCOUN ADMIN LEVEL ROLE
+/*-- ALWAYS USE ACCOUN ADMIN LEVEL ROLE
 USE ROLE ACCOUNTADMIN;
 
 -- CREATING A SMALL WAREHOUSE FOR COMPUTE NEEDS
@@ -38,7 +38,7 @@ alter git repository SNOWCONES_DEVOPS_POC.POC.GIT_REPO_STG fetch;
 show git branches in GIT_REPO_STG;
 
 -- listing the files from repo:
-list @GIT_REPO_STG/branches/;
+list @GIT_REPO_STG/branches/;*/
 
 -- executing the files directly from repo:
 EXECUTE IMMEDIATE FROM @git_repo_stg/branches/main/PROD_US_DOMAINS/STAGING_DATA/Tables/T_Devops_Demo.sql;
