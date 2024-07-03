12546fd4921bc4977f1fa8e1b631d38ebd95ef9a
kushandevopspoc <kushandevopspoc@gmail.com>
2024-07-03 11:44:40 -0400

    Update deployment.sql

--- deployment.sql
+++ deployment.sql
@@ -1,1 +1,1 @@
-execute immediate from @snowcones_devops_poc.poc.git_repo_stg/branches/main/PROD_US_DOMAINS/STAGING_DATA/Tables/T_Devops_Demo.sql using (database => '{{database}}', schema_nm => '{{schema_name}}');
+execute immediate from @snowcones_devops_poc.poc.git_repo_stg/branches/Development/PROD_US_DOMAINS/STAGING_DATA/Tables/T_Devops_Demo.sql using (database => '{{database}}', schema_nm => '{{schema_name}}');
